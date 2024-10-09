9501837efc945902d22a8496b7e7a5fc6a564bd9
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2024-10-10 01:17:24 +0300

    sweet alert

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,6 +9,7 @@ import {
 import { NotesService } from '../../shared/services/notes.service';
 import { Note } from '../../shared/interfaces/notes-data';
 import { SearchPipe } from '../../shared/pipes/search.pipe';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-home',
@@ -122,6 +123,29 @@ export class HomeComponent implements OnInit {
       error: (err) => {
         console.log(err);
       },
+    });
+  }
+
+  // sweetAlert
+  confirmBox(id: string) {
+    Swal.fire({
+      title: 'Are you sure want to remove?',
+      text: 'You will not be able to recover this file!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, delete it!',
+      cancelButtonText: 'No, keep it',
+    }).then((result) => {
+      if (result.value) {
+        Swal.fire(
+          'Deleted!',
+          'Your imaginary file has been deleted.',
+          'success'
+        );
+        this.deleteNote(id)
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire('Cancelled', 'Your imaginary file is safe :)', 'error');
+      }
     });
   }
 }
